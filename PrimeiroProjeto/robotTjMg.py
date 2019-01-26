9e71407c7c1169d7d43001529012f94dd622514c
Unknown <rocha_michael@live.com>
2019-01-26 21:36:02 -0200

    Projeto:TJMG
    Projeto TJMG Rafael

--- PrimeiroProjeto/robotTjMg.py
+++ PrimeiroProjeto/robotTjMg.py
@@ -8,6 +8,13 @@ browser.get('http://www.tjmg.jus.br/portal-tjmg/')
 element = Select(browser.find_element_by_name('tipoPesquisa'))
 element.select_by_index(2)
 
+element = Select(browser.find_element_by_name('comrCodigo'))
+element.select_by_index(26)
+
+browser.find_element_by_id('nomePessoa').send_keys('Michael Rocha Leles')
+
+browser.find_element_by_class_name('button-primary').submit()
+
 
 
 	
