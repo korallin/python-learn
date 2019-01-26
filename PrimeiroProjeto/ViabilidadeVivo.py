d014149c0fc416aa9c0f62e4ab144a2f670459f8
Unknown <rocha_michael@live.com>
2019-01-26 21:20:08 -0200

    Projeto:Vivo viabilidade
    Criado a inserção do login no site.

---
+++ PrimeiroProjeto/ViabilidadeVivo.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#rom selenium.webdriver.common.WebElement import WebElement
+
+
+browser = webdriver.Firefox()
+
+browser.get('https://vivocorp-parceiro.vivo.com.br/LoginAppV2/login.jsp')
+browser.find_element_by_name('username').send_keys('800539268')
+browser.find_element_by_name('password').send_keys('Stcbr@26')
+browser.find_element_by_class_name('button').submit()
+browser.submit()
