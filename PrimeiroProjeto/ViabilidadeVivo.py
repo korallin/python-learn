f3eafc24a962728ba523c5071bfbd763639ec054
Unknown <rocha_michael@live.com>
2019-01-27 07:43:21 -0200

    Projeto:VivoViabilidade
    Loguin

--- PrimeiroProjeto/ViabilidadeVivo.py
+++ PrimeiroProjeto/ViabilidadeVivo.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
+import time
 #from selenium.webdriver.common.by import By
 #rom selenium.webdriver.common.WebElement import WebElement
 
 
 browser = webdriver.Firefox()
 
-browser.get('https://vivocorp-parceiro.vivo.com.br/LoginAppV2/login.jsp')
+#browser.get('https://vivocorp-parceiro.vivo.com.br/LoginAppV2/login.jsp')
+browser.get('https://vivocorp-parceiro.vivo.com.br/vivocorp')
 browser.find_element_by_name('username').send_keys('800539268')
+time.sleep(5)
 browser.find_element_by_name('password').send_keys('Stcbr@26')
 browser.find_element_by_class_name('button').submit()
 browser.submit()
